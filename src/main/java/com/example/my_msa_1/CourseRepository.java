8d373d21143891cfb140c83fa8fb824b84d9891f
k114014-02@CORP.KOREACB.KR <k114014-02@CORP.KOREACB.KR>
2019-01-15 13:56:31 +0900

    test by m0ng

---
+++ src/main/java/com/example/my_msa_1/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.example.my_msa_1;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
+
+}
+
---
+++ src/main/java/com/example/my_msa_1/MyMsa1Application.java
@@ -0,0 +1,14 @@
+package com.example.my_msa_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMsa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMsa1Application.class, args);
+	}
+
+}
+
